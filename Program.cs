54efa31222e1b0f93f90bb3a3d5661c72baac66e
EvanSimon <41878176+EvanSimon@users.noreply.github.com>
2018-10-16 10:28:14 -0400

    Add files via upload
    Updated Capstone files

--- Program.cs
+++ Program.cs
@@ -11,48 +11,76 @@ namespace CapStone
         static void Main(string[] args)
         {
             int i;
-            
-           Console.WriteLine("Write a word to convert to pig latin: ");
+            bool exitProg = false;
 
-            const string vowels = "AEIOUaeio"; // vowels are declared and newWords is set as a list string, user input is read in and stored.
-            List<string> newWords = new List<string>();
-            string sentence = Console.ReadLine();
-
-
-
-
-            foreach (string word in sentence.Split(' ')) // string "word" and user "sentence" is split into substrings   
+            while (exitProg == false)
             {
-                string firstLetter = word.Substring(0, 1); // firstLetter, restOfWord are made into it's own substring
-                string restOfWord = word.Substring(1, word.Length - 1); 
-                int currentLetter = vowels.IndexOf(firstLetter); 
+                Console.WriteLine("Write a word to convert to pig latin: ");
 
-                if (currentLetter == -1) // if a consonants is entered first it flips it to the end and adds "ay"
+                const string vowels = "AEIOUaeio"; // vowels are declared and newWords is set as a list string, user input is read in and stored.
+                List<string> newWords = new List<string>();
+                string sentence = Console.ReadLine();
+
+
+
+
+                foreach (string word in sentence.Split(' ')) // string "word" and user "sentence" is split into substrings   
                 {
-                    newWords.Add(restOfWord + firstLetter + "ay");
+                    string firstLetter = word.Substring(0, 1); // firstLetter, restOfWord are made into it's own substring
+                    string restOfWord = word.Substring(1, word.Length - 1);
+                    int currentLetter = vowels.IndexOf(firstLetter);
+
+                    if (currentLetter == -1) // if a consonants is entered first it flips it to the end and adds "ay"
+                    {
+                        newWords.Add(restOfWord + firstLetter + "ay");
+                    }
+                    else // if a vowel is first ditected add way 
+                    {
+                        newWords.Add(word + "way");
+                    }
+
+
+
+
+
+
+
                 }
-                else // if a vowel is first ditected add way 
+
+
+
+                for (i = 0; i < newWords.Count; i++) // the for loop goes as long many charictures new word   
                 {
-                    newWords.Add(word + "way");  
+                    Console.WriteLine(" " + newWords[i]); // writes out new word and YOU NEED TO MACH YOU LOOP INDEX I
                 }
-                
-                
-                
-          
+
+
+                Console.WriteLine("Would you like to continue y/n ?");
+                string exit = Console.ReadLine();
+                if (exit == "y" || exit == "Y")
+
+                {
+                    exitProg = false;
+                    continue;
+                }
+
+                else if (exit == "n" || exit == "N")
+                {
+                    exitProg = true;
+                    break;
+                }
+
+
+                else if (exit != "y" || exit != "n" || exit != "Y" || exit != "Y")
+                {
+                    Console.WriteLine("Would you like to continue y/n ?");
+                    exit = Console.ReadLine();
+                    continue;
+                }
 
 
 
             }
-
-            
-
-            for (i = 0; i < newWords.Count; i++) // the for loop goes as long many charictures new word   
-            {
-                Console.Write( " " + newWords[i]); // writes out new word and YOU NEED TO MACH YOU LOOP INDEX I
-            }
-
-                    
-
 
         }    
     }
